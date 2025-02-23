468d9e9cbed1d3c54b3ed5cff7befb5bb787a0af
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-23 16:13:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
-
 let userInput = document.getElementById('ip');
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 
-btn.addEventListener("onclick" , ()=>{
-	let val = +userInput.value
-	let p = document.createElement();
-	new Promise((resolve,reject) => {
-		setTimeout(()=>{
-			p.innerText = `Result: ${val}`
-			output.append(p);
-		},1000*2)
-		resolve(val);
-	})
-	.then((val)=>{
-		val = val*2;
-		new Promise((resolve,reject) => {
-		setTimeout(()=>{
-			p.innerText = `Result: ${val}`
-			output.append(p);
-		},1000*2)
-		resolve(val);
-	})
-	.then((val)=>{
-		val = val-3;
-		new Promise((resolve,reject) => {
-		setTimeout(()=>{
-			p.innerText = `Result: ${val}`
-			output.append(p);
-		},1000*1)
-		resolve(val);
-	})
-	.then((val)=>{
-		val = val/2;
-		new Promise((resolve,reject) => {
-		setTimeout(()=>{
-			p.innerText = `Result: ${val}`
-			output.append(p);
-		},1000*1)
-		resolve(val);
-	})
-	.then((val)=>{
-		val = val+10;
-		new Promise((resolve,reject) => {
-		setTimeout(()=>{
-			p.innerText = `Final Result: ${val}`
-			output.append(p);
-		},1000*1)
-		resolve(val);
-	})
-	.catch(error => {
-		console.log(error);
-	})
+btn.addEventListener("click", () => {
+    let val = +userInput.value;
+    let p = document.createElement("p");
 
-	
-})
-
-
-
-
+    new Promise((resolve) => {
+        setTimeout(() => {
+            val = val; // Initial value
+            p.innerText = `Result: ${val}`;
+            output.append(p);
+            resolve(val);
+        }, 1000 * 2);
+    })
+    .then((val) => {
+        val *= 2;
+        p = document.createElement("p");
+        setTimeout(() => {
+            p.innerText = `Result: ${val}`;
+            output.append(p);
+        }, 1000 * 2);
+        return val;
+    })
+    .then((val) => {
+        val -= 3;
+        p = document.createElement("p");
+        setTimeout(() => {
+            p.innerText = `Result: ${val}`;
+            output.append(p);
+        }, 1000 * 1);
+        return val;
+    })
+    .then((val) => {
+        val /= 2;
+        p = document.createElement("p");
+        setTimeout(() => {
+            p.innerText = `Result: ${val}`;
+            output.append(p);
+        }, 1000 * 1);
+        return val;
+    })
+    .then((val) => {
+        val += 10;
+        p = document.createElement("p");
+        setTimeout(() => {
+            p.innerText = `Final Result: ${val}`;
+            output.append(p);
+        }, 1000 * 1);
+        return val;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+});
